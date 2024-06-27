69852e4818d943359ee223415f3e726ee374d53a
akberofh <sd85bejal@code.edu.az>
2024-06-27 22:18:30 +0400

    commit

--- ChatApp/front-end/src/components/ChatContainer.jsx
+++ ChatApp/front-end/src/components/ChatContainer.jsx
@@ -4,6 +4,7 @@ import ChatLists from "./ChatLists";
 import InputText from "./InputText";
 import UserLogin from "./UserLogin";
 import socketIOClient from "socket.io-client";
+import { FaGithub } from "react-icons/fa";
 
 const ChatContainer = () => {
   const [user, setUser] = useState(localStorage.getItem("user"));
@@ -47,7 +48,7 @@ const ChatContainer = () => {
           <div className="chats_header">
             <h4>Username: {user}</h4>
             <p>
-              <FaYoutube className="chats_icon" /> Code With Yousaf
+              <FaGithub className="chats_icon" /> Code With Akberofh
             </p>
             <p className="chats_logout" onClick={Logout}>
               <strong>Logout</strong>
