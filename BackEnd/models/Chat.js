2bc12bbd4115d49f342d78b0b3c713fad2312009
akberofh <sd85bejal@code.edu.az>
2024-06-28 07:28:27 +0400

    commit

---
+++ BackEnd/models/Chat.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+
+const chatSchema = new mongoose.Schema({
+    username: String,
+    message: String,
+    avatar: String,
+    timestamp: { type: Date, default: Date.now }
+});
+
+const Chat = mongoose.model('message', chatSchema);
+
+export default Chat;
